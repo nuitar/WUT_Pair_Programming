fa6ce2844bfcf5596c8d0d7481a1c7fa82337a73
nuitar <1397231507@qq.com>
2023-05-25 16:02:30 +0800

    init

---
+++ header/gridmap.h
@@ -0,0 +1,46 @@
+#ifndef GRIDMAP_H
+#define GRIDMAP_H
+
+#include <QWidget>
+
+
+#include <QWidget>
+#include <QPen>
+#include <QPainter>
+#include <QPaintEvent>
+
+class gridMap : public QWidget
+{
+    Q_OBJECT
+
+public:
+    gridMap(QWidget *parent = nullptr);
+    ~gridMap();
+
+protected:
+    void paintEvent(QPaintEvent *event);
+    void wheelEvent(QWheelEvent *event);
+    void resizeEvent(QResizeEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+
+
+
+private:
+    void setBackgroundColor();              // 设置窗口背景颜色
+    void drawRowLines(QPainter *painter);   // 绘制横线
+    void drawColLines(QPainter *painter);   // 绘制竖线
+    void drawCell(QPainter *painter);
+
+
+private:
+    int minGap;                              // 最小线间隔
+    int maxGap;                              // 最大线间隔
+    float gap;                               // 当前线间隔
+    float startX;                            // 竖线初始x坐标（从窗口中间开始）
+    float startY;                            // 横线初始y坐标（从窗口中间开始）
+    QPen darkPen;                            // 粗一点的画笔
+    QVector<QVector<bool>> mat;
+};
+
+
+#endif // GRIDMAP_H
---
+++ resource/gridmap.cpp
@@ -0,0 +1,149 @@
+
+#include "gridmap.h"
+#include <QColor>
+#include <QPalette>
+#include <QPointF>
+
+gridMap::gridMap(QWidget *parent)
+    : QWidget(parent)
+{
+
+    this->minGap = 4;
+    this->maxGap = 14;
+    this->gap = 20;
+    this->startX = this->width() / 2;
+    this->startY = this->height() / 2;
+    this->darkPen = QPen(QColor(200, 200, 200));
+    this->darkPen.setWidth(1);
+    this->setBackgroundColor();
+    this->mat = QVector<QVector<bool>>(26, QVector<bool>(40, 0));
+    qDebug() << startX << "," << startY;
+}
+
+gridMap::~gridMap()
+{
+
+}
+
+void gridMap::setBackgroundColor()
+{
+    QPalette palette;
+    palette.setColor(QPalette::ColorRole::Window, QColor(140, 140, 140));
+    this->setPalette(palette);
+    this->setAutoFillBackground(true);
+}
+
+void gridMap::drawRowLines(QPainter *painter)
+{
+    float biggerY = this->startY;
+    float smallerY = this->startY;
+
+    painter->setPen(this->darkPen);
+
+    while (true)
+    {
+        painter->drawLine(QPointF(0.0, biggerY), QPointF(this->width(), biggerY));
+        painter->drawLine(QPointF(0.0, smallerY), QPointF(this->width(), smallerY));
+        biggerY += this->gap;
+        smallerY -= this->gap;
+        if (smallerY <= 0 || biggerY >= this->height())
+        {
+            break;
+        }
+        painter->setPen(this->darkPen);
+    }
+
+}
+
+void gridMap::drawColLines(QPainter *painter)
+{
+    float biggerX = this->startX;
+    float smallerX = this->startX;
+
+    painter->setPen(this->darkPen);
+
+    while (true)
+    {
+        painter->drawLine(QPointF(biggerX, 0.0), QPointF(biggerX, this->height()));
+        painter->drawLine(QPointF(smallerX, 0.0), QPointF(smallerX, this->height()));
+        biggerX += this->gap;
+        smallerX -= this->gap;
+        if (smallerX <= 0 || biggerX >= this->width())
+        {
+            break;
+        }
+        painter->setPen(this->darkPen);
+
+    }
+
+}
+
+void gridMap::drawCell(QPainter *painter)
+{
+
+    int l = this->gap;
+    QColor cTrue(231, 250, 65), cFalse(140, 140, 140);
+    for(int i = 0; i < mat.size(); i++)
+    {
+        for(int j = 0; j < mat[0].size(); j++)
+        {
+            QColor c = mat[i][j] ? cTrue : cFalse;
+            painter->setPen(QPen(c));
+            painter->setBrush(QBrush(c));
+            painter->drawRect(j * this->gap, i * this->gap,  l, l);
+        }
+
+    }
+
+}
+
+void gridMap::paintEvent(QPaintEvent *event)
+{
+    QWidget::paintEvent(event);
+    QPainter painter(this);
+    this->drawCell(&painter);
+
+    this->drawRowLines(&painter);
+    this->drawColLines(&painter);
+}
+
+void gridMap::wheelEvent(QWheelEvent *event)
+{
+    QWidget::wheelEvent(event);
+//    if (event->angleDelta().y() > 0)
+//    {
+//        this->gap += 0.1;
+//    }
+//    else if (event->angleDelta().y() < 0)
+//    {
+//        this->gap -= 0.1;
+//    }
+
+//    if (this->gap >= this->maxGap)
+//    {
+//        this->gap = this->minGap;
+//    }
+//    else if (this->gap <= this->minGap)
+//    {
+//        this->gap = this->maxGap;
+//    }
+
+    this->update();
+}
+
+void gridMap::resizeEvent(QResizeEvent *event)
+{
+    QWidget::resizeEvent(event);
+    this->startX = this->width() / 2;
+    this->startY = this->height() / 2;
+    this->update();
+}
+
+void gridMap::mousePressEvent(QMouseEvent *event)
+{
+    QPainter painter(this);
+    QPoint p = event->pos();
+    int x = p.x() / this->gap, y = p.y() / this->gap ;
+    mat[y][x] = !mat[y][x];
+    this->update();
+}
