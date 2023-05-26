ca8c2ba2565cccb92ff0255be21c254a70cb0fe8
wyxwyxwyxwyx <11668157+wyxwyxwyxwyx@user.noreply.gitee.com>
2023-05-26 05:09:27 +0000

    update Logic.h.
    Signed-off-by: wyxwyxwyxwyx <11668157+wyxwyxwyxwyx@user.noreply.gitee.com>

--- Logic.h
+++ Logic.h
@@ -11,8 +11,9 @@ private:
 public:
 	Logic();
 	int getRoundAlive(int i, int j);   //获取第i行j列的当前的格子的周围的活细胞的数目
-	vector<MyPoint> NextState();   //下一个状态
+	void NextState();   //下一个状态
 	void Click(MyPoint point);   //点击鼠标则将当前格子取反，更新地图。
-	vector<MyPoint> Random();  //随机生成一组活细胞的坐标
+	void Random();  //随机生成一组活细胞的坐标
 	void Reset();  //清空数组
+	vector<MyPoint> GetTruePoint();   //返回地图中为1的所有坐标点
 };
